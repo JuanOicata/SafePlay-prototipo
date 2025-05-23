9cf0f4ddf5a4715045a2033caa6f24c1446bb9de
Juan Oicata <jeotoicata@gmail.com>
2025-05-22 20:27:46 -0500

    first commit

---
+++ src/test/java/Ucentral/SafePlay_ver01/ApplicationTests.java
@@ -0,0 +1,13 @@
+package Ucentral.SafePlay_ver01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
